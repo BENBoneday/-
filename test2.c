d274873f2ce294e4ce9df18c8e32f96af0fd573a
BENBoneday <85631569+BENBoneday@users.noreply.github.com>
2021-11-18 23:52:27 +0800

    单链表的实现

---
+++ test2.c
@@ -0,0 +1,35 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include"singleList.h"
+//void test()
+//{
+//	Node* s=NULL;
+//	SListPushBack(&s, 1);
+//	SListPushBack(&s, 2);
+//	SListPushBack(&s, 3);
+//	SListPushBack(&s, 4);
+//	SListPrint(s);
+//	SListPopBack(&s);
+//	//SListPopBack(&s);
+//	//SListPopBack(&s);
+//	SListPopBack(&s);
+//	SListPrint(s);
+//	SListPushFront(&s, 66);
+//	SListPushFront(&s, 66);
+//	SListPushFront(&s, 66);
+//	SListPushFront(&s, 66);
+//	SListPushFront(&s, 66);
+//	SListPrint(s);
+//	SListPopFront(&s);
+//	SListPopFront(&s);
+//	SListPopFront(&s);
+//	SListPrint(s);
+//	SListInsertAfter(s, 888);
+//	SListPrint(s);
+//	SListEraseAfter(s);
+//	SListPrint(s);
+//}
+//int main()
+//{
+//	test();
+//	return 0;
+//}
