34d84247b61a487a69cf7f895c36c3e44e5d3183
BENBoneday <85631569+BENBoneday@users.noreply.github.com>
2021-11-21 22:02:01 +0800

    插入排序基本实现

---
+++ 插入排序.c
@@ -0,0 +1,58 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//#include<time.h>
+//#include<string.h>
+//#include<sys/timeb.h>
+//
+//#define MAX 10
+//long getSystemTime()
+//{
+//	struct timeb tb;
+//	ftime(&tb);
+//	return tb.time * 1000 + tb.millitm;
+//}
+//void Print(int arr[], int n)
+//{
+//	int i = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//}
+//void InsertSort(int arr[],int length)
+//{
+//	int j;
+//	for (int i = 1; i < length; i++)
+//	{
+//		if (arr[i] < arr[i - 1])
+//		{
+//			int temp = arr[i];
+//			for (j = i - 1; j >= 0 && temp < arr[j]; j--)
+//			{
+//				arr[j + 1] = arr[j];
+//			}
+//			arr[j + 1] = temp;
+//		}
+//	}
+//}
+//int main()
+//{
+//	int arr[MAX];
+//	srand((unsigned int)time(NULL));
+//	for (int i = 0; i < MAX; i++)
+//	{
+//		int randNum = rand() % MAX;
+//		arr[i] = randNum;
+//	}
+//	Print(arr, MAX);
+//	InsertSort(arr,MAX);
+//	Print(arr, MAX);
+//	return 0;
+//}
+int main()
+{
+	int* p, a = 0;
+	printf("%d", a);
+	return 0;
+}
